c8568bea7f761e43e70ff97898f47280942520f0
peimengsui <peimengsui@gmail.com>
2017-11-13 16:50:18 -0500

    fix bug

---
+++ siamese_lstm/siamese_lstm.py
@@ -0,0 +1,66 @@
+import sys
+import os
+sys.path.append(os.pardir)
+import torch
+import torch.autograd as ta
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as opt
+from torch.autograd import Variable
+
+class WordLSTM(nn.Module):
+    def __init__(self, word_vecs, rsize = 50):
+        super(WordLSTM, self).__init__()
+        #typical Pytorch cudnn LSTM declaration
+        self.embedding = nn.Embedding(106687, 300, padding_idx=1) 
+        self.embedding.weight.data.copy_(torch.FloatTensor(word_vecs))
+        self.embedding.weight.requires_grad = False
+        self.rnn = nn.LSTM(input_size=300, 
+                           hidden_size=rsize, 
+                           batch_first=True, 
+                          )
+
+    def forward(self, x):
+        #pass the embedding input sequence through the LSTM
+        #h_0 is stand in for the initial hidden states, which is always 0 in this case
+        h0 = Variable(torch.zeros(1, x.size()[0], 50))
+        x = self.embedding(x)
+        if torch.cuda.is_available():
+            h0 = h0.cuda()
+        fseq, (h_n, c_n) = self.rnn(x, (h0, h0))
+        
+        #there are several ways we can pool this, but just taking the last hidden state works fine enough for such a problem
+        x = fseq[:,-1,:]
+        
+        return x
+
+#A siamese network takes two different sets of data, processes them through two identical branches
+#deploys some fancy merging function and minimizes a final metric between the two.
+#Similar samples should have a low metric, dissimilar samples should have a large metric.
+#Check out my (one and only) paper on this exact topic:
+#https://www.researchgate.net/publication/304834009_Learning_Text_Similarity_with_Siamese_Recurrent_Networks
+#although in this case the metric is a probability :)
+class Siamese(nn.Module):
+    def __init__(self, word_vecs):
+        super(Siamese, self).__init__()
+        self.word_lstm_encoder = WordLSTM(word_vecs)
+        #The final classifier is a typical feedforward NN with one categorical output
+    def forward(self, x, y):
+        
+        #we take two identical branches of parameters and feed them two different datas
+        #first on the word lstm branches
+        x = self.word_lstm_encoder(x)
+        y = self.word_lstm_encoder(y)
+        
+        #compute a distance metric. This can be tricky and needs experimenting for different tasks.
+        #Here we use two: a product between the embeddings and an euclidean distance
+        #And do it separately for words and characters
+        #You can also, in principle, just concatenate the outputs, but the branches together
+        #but then you need to compute them twice (also feed y's through the x branch and x's through the y branch
+        #to ensure the readout (classifier) comes out symmetrical and the sentences transitve
+        
+        #z = torch.cat([x*y, (x-y)**2, cx*cy, (cx-cy)**2], 1)
+        z = torch.exp(-torch.sum(torch.abs(x - y), dim = 1))
+        #finally output the probability that these two questions mean the same thing
+        return z
+        
---
+++ siamese_lstm/siamese_main.py
@@ -0,0 +1,99 @@
+import sys
+import os
+sys.path.append(os.pardir)
+from utilities import data_loader
+import torch
+import torch.autograd as ta
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as opt
+from siamese_lstm import Siamese
+from torch.autograd import Variable
+import numpy as np
+
+data_dir = '../data'
+vocabulary, word_embeddings, word_to_index_map, index_to_word_map = data_loader.load_embed(data_dir + '/wordvec.txt')
+training_set = data_loader.load_data(data_dir+'/train.tsv', word_to_index_map)
+valid_set = data_loader.load_data(data_dir+'/dev.tsv', word_to_index_map)
+test_set = data_loader.load_data(data_dir+'/test.tsv', word_to_index_map)
+
+train_loader = data_loader.batch_iter(training_set, 32)
+valid_loader = data_loader.batch_iter(valid_set, 32)
+test_loader = data_loader.batch_iter(test_set, 1)
+
+def test(batch_size, model, data_iter, criterion, test_set, epoch):
+    step = 1
+    losses = []
+    correct = 0
+    total_batches = int(len(test_set) / batch_size)
+    while True:
+        model.eval()
+        label, sentence_a_words, sentence_b_words = next(data_iter) 
+        label = Variable(torch.FloatTensor(label))
+        sentence_a_words = Variable(torch.LongTensor(sentence_a_words))
+        sentence_b_words = Variable(torch.LongTensor(sentence_b_words))
+        if torch.cuda.is_available():
+            label = label.cuda()
+            sentence_a_words = sentence_a_words.cuda()
+            sentence_b_words = sentence_b_words.cuda()
+        output = model(sentence_a_words, sentence_b_words)
+        pred = (output.data>0.5).float()
+        correct += pred.eq(label.data.view_as(pred)).cpu().sum()
+        loss = criterion(output, label)
+        losses.append(loss.data[0])
+        if step % total_batches == 0:
+            print( "Epoch:", (epoch), "Avg test Loss:", np.mean(losses), "Test Accuracy:", correct/(batch_size*total_batches))
+            correct = 0
+            return 
+        step += 1
+
+def training_loop(batch_size, model, optim, train_loader, valid_loader, num_epochs, criterion, training_set, valid_set):
+    step = 1
+    epoch = 1
+    losses = []
+    correct = 0
+    best_acc = 0
+    total_batches = int(len(training_set) / batch_size)
+    while epoch <= num_epochs:
+        model.train()
+        label, sentence_a_words, sentence_b_words = next(train_loader) 
+        label = Variable(torch.FloatTensor(label))
+        sentence_a_words = Variable(torch.LongTensor(sentence_a_words))
+        sentence_b_words = Variable(torch.LongTensor(sentence_b_words))
+        if torch.cuda.is_available():
+            label = label.cuda()
+            sentence_a_words = sentence_a_words.cuda()
+            sentence_b_words = sentence_b_words.cuda()
+        model.zero_grad()
+        output = model(sentence_a_words, sentence_b_words)
+        pred = (output.data>0.5).float()
+        correct += pred.eq(label.data.view_as(pred)).cpu().sum()
+        loss = criterion(output, label)
+        losses.append(loss.data[0])
+        loss.backward()
+        optim.step()
+        if step % total_batches == 0:
+            print( "Epoch:", (epoch), "Training Avg Loss:", np.mean(losses), "Training Accuracy:", correct/(batch_size*total_batches))
+            acc = test(batch_size, model, valid_loader, criterion, valid_set, epoch)
+            if acc > best_acc:
+                best_acc = acc
+                torch.save(model.state_dict(), 'best_siamese.pt')
+            correct = 0
+            losses = []
+            epoch += 1
+        step += 1
+
+num_epochs = 50
+model = Siamese(word_embeddings)
+if torch.cuda.is_available():
+    model.cuda()
+para1 = filter(lambda p: p.requires_grad, model.parameters())
+optim = opt.Adam(para1)
+criterion = torch.nn.BCELoss()
+training_loop(32, model, optim, train_loader, valid_loader, num_epochs, criterion, training_set, valid_set)
+
+model = Siamese(word_embeddings)
+model.load_state_dict(torch.load(best_siamese.pt))
+if torch.cuda.is_available():
+    model.cuda()
+test(1, model, test_loader, criterion, test_set, num_epochs)
